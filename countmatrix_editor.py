781a3a422c2aff9435832e3edcdc8f6fb8ef4f3d
zstone00000 <108751984+zstone00000@users.noreply.github.com>
2023-06-16 13:50:17 -0400

    Add files via upload

--- countmatrix_editor.py
+++ countmatrix_editor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy import sparse as sp
+from scipy.sparse import hstack
 
 def filter_countmatrix(keep_vocab, countmatrix):
     """
@@ -48,56 +48,42 @@ def quotient_countmatrix(eqreldict, vocabtable, countmatrix):
                 New vocab table and count matrix where words in the equivalence relation have been identified.
                 The chosen representative will represent the equivalence class.
     """
-#Can probably simplified creation of vocab in first case by just creating a vocab table and resetting index    
-    # print('Creating vocab dictionaries')
     w2c = {vocabtable.loc[k,'word']:vocabtable.loc[k,'col'] for k in range(vocabtable.shape[0])}
     c2w = {v:k for k,v in w2c.items()}
     
     merged_cols = list()
-    new_w2c = dict()
-    
-    
+    merged_vocab = list()
     droplist = list()
-    for index, key in enumerate(eqreldict.keys()):
-        # print(f'Creating {key} column')
-        droplist.append(key)
-        new_w2c[key] = index
     
+    for key, value in eqreldict.items():
+        droplist.append(key)
+        merged_vocab.append(key)
         newcol = countmatrix.getcol(w2c[key])
         
         values = eqreldict[key]
         for value in values:
-            # print(f'Adding value for {value}')
             droplist.append(value)
-            
             newcol += countmatrix.getcol(w2c[value])
-        # print(f'Appending {key} eq class to matrix')
+
         merged_cols.append(newcol)
 
-    # print('merging columns')
-    merged_cols = sp.hstack(merged_cols)    
+
+    merged_cols = hstack(merged_cols)    
     nmerged = merged_cols.shape[1]
-    # print('creating merged vocab')
-    mergedvocab = pd.DataFrame([(k,v) for k,v in new_w2c.items()])
-    mergedvocab.columns = ['word', 'col']
-    mergedvocab = mergedvocab.sort_values(by = 'col').reset_index(drop=True)
+    merged_vocab = pd.Series(merged_vocab).reset_index()
+    merged_vocab.columns = ['col', 'word']
+    merged_vocab = merged_vocab.sort_values(by = 'col').reset_index(drop=True)
     
-    # print('creating subset component')
     keep_table = vocabtable[~vocabtable['word'].isin(droplist)].copy()
     subvocab, subset = filter_countmatrix(keep_table, countmatrix)
     subvocab = subvocab[['col','word']].copy()
     subvocab['col'] = subvocab['col'] + nmerged
-    
-    # print('combining vocabs')
-    newvocab = pd.concat([mergedvocab,subvocab], axis = 0).reset_index(drop=True)
-    # print('combining count matrices')
-    newmatrix = sp.hstack([merged_cols,subset])
+
+    newvocab = pd.concat([merged_vocab,subvocab], axis = 0).reset_index(drop=True)
+    newmatrix = hstack([merged_cols,subset])
     
     return newvocab, newmatrix
-
-
-
-      
+  
 def w2c_totable(w2cdict):
     vocab = pd.DataFrame([(k,v) for k,v in w2cdict.items()])
     vocab.columns = ['word', 'col']
